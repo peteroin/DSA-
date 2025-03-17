55e112689e2cbe8d931fc7b383fce83150757bb4
peteroin <peter.mygit@gmail.com>
2025-03-17 23:18:07 +0530

    feat: basic c program

---
+++ linkedListCreateDisplayReverse.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert a node at the end of the linked list
+void append(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head_ref; 
+
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = new_node;
+}
+
+// Function to display the linked list
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to reverse the linked list
+void reverse(struct Node** head_ref) {
+    struct Node* prev = NULL;
+    struct Node* current = *head_ref;
+    struct Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next; // Store next node
+        current->next = prev; // Reverse current node's pointer
+        prev = current;       // Move prev to current
+        current = next;       // Move current to next
+    }
+    *head_ref = prev; // Update head to new first node
+}
+
+// Driver function
+int main() {
+    struct Node* head = NULL;
+    int n, value;
+
+    // Get the number of nodes
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    // Get user input to create the linked list
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        append(&head, value);
+    }
+
+    printf("\nOriginal Linked List:\n");
+    display(head);
+
+    // Reverse the linked list
+    reverse(&head);
+
+    printf("\nReversed Linked List:\n");
+    display(head);
+
+    return 0;
+}
