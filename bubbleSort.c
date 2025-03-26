a7746c703106bf1477cb11d01490f6e77e82f8e5
peteroin <peter.mygit@gmail.com>
2025-03-26 21:25:15 +0530

    feat: basic c program

---
+++ bubbleSort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; // To check if any swapping happens
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        // If no swapping happened, the array is already sorted
+        if (swapped == 0)
+            break;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
