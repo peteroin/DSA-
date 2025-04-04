aeb27e56763c6bea25d1c9b02788453675108fd4
peteroin <peter.mygit@gmail.com>
2025-04-04 20:10:06 +0530

    feat: basic c program

---
+++ infix_to_postfix.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+// Stack structure
+typedef struct {
+    int top;
+    char items[MAX];
+} Stack;
+
+// Function to initialize stack
+void initStack(Stack *s) {
+    s->top = -1;
+}
+
+// Function to check if stack is empty
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
+
+// Function to check if stack is full
+int isFull(Stack *s) {
+    return s->top == MAX - 1;
+}
+
+// Function to push an element onto the stack
+void push(Stack *s, char c) {
+    if (!isFull(s)) {
+        s->items[++(s->top)] = c;
+    } else {
+        printf("Stack Overflow\n");
+    }
+}
+
+// Function to pop an element from the stack
+char pop(Stack *s) {
+    if (!isEmpty(s)) {
+        return s->items[(s->top)--];
+    } else {
+        return '\0';
+    }
+}
+
+// Function to get the top element of stack without popping
+char peek(Stack *s) {
+    if (!isEmpty(s)) {
+        return s->items[s->top];
+    } else {
+        return '\0';
+    }
+}
+
+// Function to check precedence of operators
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+// Function to check if character is an operator
+int isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+
+// Function to convert infix expression to postfix
+void infixToPostfix(char *infix, char *postfix) {
+    Stack s;
+    initStack(&s);
+    int i, j = 0;
+    char token;
+
+    for (i = 0; infix[i] != '\0'; i++) {
+        token = infix[i];
+
+        // If operand, add to postfix expression
+        if (isalnum(token)) {
+            postfix[j++] = token;
+        }
+        // If left parenthesis, push to stack
+        else if (token == '(') {
+            push(&s, token);
+        }
+        // If right parenthesis, pop from stack until left parenthesis is found
+        else if (token == ')') {
+            while (!isEmpty(&s) && peek(&s) != '(') {
+                postfix[j++] = pop(&s);
+            }
+            pop(&s); // Remove '(' from stack
+        }
+        // If operator, pop operators of higher precedence and then push current operator
+        else if (isOperator(token)) {
+            while (!isEmpty(&s) && precedence(peek(&s)) >= precedence(token)) {
+                postfix[j++] = pop(&s);
+            }
+            push(&s, token);
+        }
+    }
+
+    // Pop remaining operators in stack
+    while (!isEmpty(&s)) {
+        postfix[j++] = pop(&s);
+    }
+
+    postfix[j] = '\0'; // Null terminate postfix expression
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
