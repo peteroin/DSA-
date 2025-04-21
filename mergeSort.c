7db93709d569bf61a6951bd023625f9dd5ed13b4
peteroin <peter.mygit@gmail.com>
2025-04-21 20:03:18 +0530

    feat: basic c program

--- mergeSort.c
+++ mergeSort.c
@@ -49,7 +49,6 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
-// Function to print an array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
